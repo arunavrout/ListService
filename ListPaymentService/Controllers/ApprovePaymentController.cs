1bf3b996de61dd8eb9cdf7c3e28ab2ed57d311b4
kumar arunav rout <arunav.rout93@gmail.com>
2017-03-24 10:04:24 +0530

    initial

---
+++ ListPaymentService/Controllers/ApprovePaymentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using ListPaymentService.Models;
+using ListPaymentService.Providers;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ListPaymentService.Controllers
+{
+    public class ApprovePaymentController : Controller
+    {
+        // GET: ApprovePayment
+
+        public class ApproveController : ApiController
+        {
+            ApproveResponse res = null;
+            [System.Web.Http.HttpPost]
+            public async Task<IHttpActionResult> MakePayment([FromBody] ApproveRequesst payment)
+            {
+                try
+                {
+                    ApprovePayment client = new ApprovePayment();
+                    res = await client.ApprovePayments(payment);
+                }
+                catch (Exception)
+                {
+                    return StatusCode(HttpStatusCode.BadRequest);
+                }
+                return Ok(res);
+            }
+        }
+
+    }
+}
